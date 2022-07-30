6053f1ecef0d8753131123f6d9a473c5a18a6304
noahsb96 <noahsb96@yahoo.com>
2022-07-30 00:03:57 -0600

    spawned bug png, adding animation

--- js/app.js
+++ js/app.js
@@ -16,12 +16,54 @@ const startButton = document.querySelector('.start-button');
 const player1 = document.querySelector('#player1');
 const player2 = document.querySelector('#player2');
 const h1 = document.querySelector('h1');
-const player1score = document.querySelector('#player1score')
-const player2score = document.querySelector('#player2score')
+const player1score = document.querySelector('#player1score');
+const player2score = document.querySelector('#player2score');
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+class Image {
+    constructor(imagePath, xpos, ypos, width, height, speed) {
+        this.imagePath = imagePath;
+        this.xpos = xpos;
+        this.ypos = ypos;
+        this.width = width;
+        this.height = height;
+        this.speed = speed;
+    }
+
+    createImage(context, imagePath, xpos, ypos, width, height) {
+        let myImage = document.createElement('img');
+        myImage.src = imagePath;
+        myImage.onload = function() {
+            context.drawImage(myImage, xpos, ypos, width, height);
+        }
+    }
+
+    drawGrasshopper () {
+        canvas.style.width = document.body.clientWidth
+        canvas.style.height = document.body.clientHeight
+        const image = new Image('grasshopper.png', 50, 50, 80, 80);
+        createImage(context, image.imagePath, image.xpos, image.ypos, image.width, image.height);
+    }
+
+    update() {
+        this.drawGrasshopper()
+    }
+}
+
+let bugCounter = 1;
+
+let allBugs = [];
+
+let createGrasshopper = function(grasshopper) {
+    Image.draw(context);
+}
+
+for (let n)
 
 function timer() {
     if (time > 0) {
-        time -= 1
+        time -= 1;
         h1.innerHTML = `Time: ${time}`;
     }
 }
@@ -38,4 +80,5 @@ function startGame() {
 }
 
 startButton.addEventListener('click', startGame);
-startButton.addEventListener('click', timer)
+startButton.addEventListener('click', timer);
+startButton.addEventListener('click', drawGrasshopper);
