37fa5ebde8cb5ebd3b145f626fc76a5f7ad052d4
noahsb96 <noahsb96@yahoo.com>
2022-08-01 02:23:15 -0600

    Added player movement and wall boundaries

--- js/app.js
+++ js/app.js
@@ -13,8 +13,8 @@
 
 let time = 60;
 const startButton = document.querySelector('.start-button');
-const player1 = document.querySelector('#player1');
-const player2 = document.querySelector('#player2');
+const player1 = document.getElementById('player1');
+const player2 = document.getElementById('player2');
 const h1 = document.querySelector('h1');
 const player1score = document.querySelector('#player1score');
 const player2score = document.querySelector('#player2score');
@@ -25,6 +25,103 @@ const window_height = window.innerHeight
 const window_width = window.innerWidth
 canvas.width = window_width
 canvas.height = window_height
+
+const players = {
+    firstPlayer: {
+        width:150,
+        height:150,
+        xpos:0,
+        ypos: canvas.height / 1.2,
+        speed: 5,
+    },
+    secondPlayer: {
+        width:150,
+        height:150,
+        xpos: canvas.width / 1.09,
+        ypos: canvas.height / 1.2,
+        speed: 5,
+    },
+
+    drawPlayer(){
+        context.drawImage(player1, this.firstPlayer.xpos, this.firstPlayer.ypos, this.firstPlayer.width, this.firstPlayer.height)
+        context.drawImage(player2, this.secondPlayer.xpos, this.secondPlayer.ypos, this.secondPlayer.width, this.secondPlayer.height)
+    },
+}
+
+let leftKeyPress = false;
+let rightKeyPress = false;
+let zKeyPress = false;
+let cKeyPress = false;
+
+const leftKey = 37
+const rightKey = 39
+const zKey = 90
+const cKey = 67
+
+function keyPressed(evt) {
+    if(evt.keyCode == leftKey) {
+        leftKeyPress = true
+    }
+    if(evt.keyCode == rightKey) {
+        rightKeyPress = true
+    }
+    if(evt.keyCode == zKey) {
+        zKeyPress = true
+    }
+    if(evt.keyCode == cKey) {
+        cKeyPress = true
+    }
+}
+
+function keyReleased(evt) {
+    if(evt.keyCode == leftKey) {
+        leftKeyPress = false
+    }
+    if(evt.keyCode == rightKey) {
+        rightKeyPress = false
+    }
+    if(evt.keyCode == zKey) {
+        zKeyPress = false
+    }
+    if(evt.keyCode == cKey) {
+        cKeyPress = false
+    }
+}
+
+function rightWall() {
+    if(players.firstPlayer.xpos + players.firstPlayer.width > canvas.width){
+        players.firstPlayer.xpos = canvas.width - players.firstPlayer.width
+        
+    }
+    if(players.secondPlayer.xpos + players.secondPlayer.width > canvas.width){
+        players.secondPlayer.xpos = canvas.width - players.secondPlayer.width
+    }
+}
+
+
+function playerMove() {
+    if(zKeyPress && players.firstPlayer.xpos > 0) {
+        players.firstPlayer.xpos -= players.firstPlayer.speed
+    }
+    if(cKeyPress) {
+        players.firstPlayer.xpos += players.firstPlayer.speed
+    }
+    if(leftKeyPress && players.secondPlayer.xpos > 0) {
+        players.secondPlayer.xpos -= players.secondPlayer.speed
+    }
+    if(rightKeyPress) {
+        players.secondPlayer.xpos += players.secondPlayer.speed
+    }
+    rightWall()
+}
+
+let updateElements = function() {
+    game.clear()
+    players.drawPlayer()
+    playerMove()
+    bugMovement()
+    requestAnimationFrame(updateElements)
+}
 
 function timer() {
     if (time > 0) {
@@ -42,8 +139,6 @@ const game = {
             timer()
         }, 1000)
         startButton.classList.add('hidden');
-        player1.classList.remove('hidden');
-        player2.classList.remove('hidden');
         player1score.classList.remove('hidden');
         player2score.classList.remove('hidden');
         canvas.classList.remove('hidden')
@@ -87,15 +182,13 @@ const bugMovement = () => {
     })
 }
 
-let updateBug = function() {
-    game.clear()
-    bugMovement()
-    requestAnimationFrame(updateBug)
-}
-
-startButton.addEventListener('click', game.startGame);
-startButton.addEventListener('click', timer);
-startButton.addEventListener('click', updateBug)
+document.addEventListener('keydown', keyPressed)
+document.addEventListener('keyup', keyReleased)
+startButton.addEventListener('click', () => { 
+    game.startGame();
+    timer()
+    updateElements()
+})
 
 // class Bug {
 //     constructor() {
