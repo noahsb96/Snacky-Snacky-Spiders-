b1b1ae2b14db6ba8477abd31b3a6f9a3c78faff3
noahsb96 <noahsb96@yahoo.com>
2022-08-02 15:53:01 -0600

    added comments

--- js/app.js
+++ js/app.js
@@ -11,218 +11,217 @@
     //alert pops up that declares the winner
 // game is reset
 
-let time = 61;
-const startButton = document.querySelector('.start-button');
-const player1 = document.getElementById('player1');
-const player2 = document.getElementById('player2');
-const bugImage = document.getElementById('bug')
-const h1 = document.querySelector('h1');
-const player1scoreText = document.querySelector('#player1score');
-const player2scoreText = document.querySelector('#player2score');
-let player1score = 0
-let player2score = 0
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
-const bugArray = []
-const window_height = window.innerHeight
-const window_width = window.innerWidth
-canvas.width = window_width
-canvas.height = window_height
+let time = 61; // Time limit
+const startButton = document.querySelector('.start-button'); // start button
+const player1 = document.getElementById('player1'); // player 1 image
+const player2 = document.getElementById('player2'); // player 2 image
+const bugImage = document.getElementById('bug') // bug image
+const h1 = document.querySelector('h1'); // header
+const player1scoreText = document.querySelector('#player1score'); // player 1 score text
+const player2scoreText = document.querySelector('#player2score'); // player 2 score text
+let player1score = 0 // player 1 score
+let player2score = 0 // player 2 score
+const canvas = document.getElementById('canvas'); // canvas
+const context = canvas.getContext('2d'); // canvas drawing
+const bugArray = [] // bug array
+const window_height = window.innerHeight // window height
+const window_width = window.innerWidth // window width
+canvas.width = window_width // canvas width
+canvas.height = window_height // canvas height
 
-const players = {
-    firstPlayer: {
-        width:150,
-        height:150,
-        xpos: 5,
-        ypos: Math.round(canvas.height / 1.2),
-        speed: 5,
+const players = { // players objects
+    firstPlayer: { // first player object
+        width:150, // player picture width
+        height:150, // player picture height
+        xpos: 5, // player picture starting x position
+        ypos: Math.round(canvas.height / 1.2), // player picture starting y position
+        speed: 5, // player speed in animation
     },
     
-    secondPlayer: {
-        width:150,
-        height:150,
-        xpos: Math.floor(Math.round(canvas.width / 5) * 5),
-        ypos: Math.round(canvas.height / 1.2),
-        speed: 5,
+    secondPlayer: { // second player object
+        width:150, // player picture width
+        height:150, // player picture height
+        xpos: Math.floor(Math.round(canvas.width / 5) * 5), // player picture x position
+        ypos: Math.round(canvas.height / 1.2), // player picture y position
+        speed: 5, // player picture speed in animation
     },
 
     drawPlayerOne() {
-        context.drawImage(player1, this.firstPlayer.xpos, this.firstPlayer.ypos, this.firstPlayer.width, this.firstPlayer.height)
+        context.drawImage(player1, this.firstPlayer.xpos, this.firstPlayer.ypos, this.firstPlayer.width, this.firstPlayer.height) // drawing player one picture
     },
 
     drawPlayerTwo() {
-        context.drawImage(player2, this.secondPlayer.xpos, this.secondPlayer.ypos, this.secondPlayer.width, this.secondPlayer.height)
+        context.drawImage(player2, this.secondPlayer.xpos, this.secondPlayer.ypos, this.secondPlayer.width, this.secondPlayer.height) // drawing player two picture
     }
 }
 
-class Bug {
+class Bug { // bug class
     constructor() {
-        this.xpos = Math.floor(Math.random() * canvas.width / 5) * 5
-        this.ypos = 0
-        this.width = 60
-        this.height = 60
-        this.speed = 5
-        this.radius = 20
-        this.dy = 1 * this.speed
+        this.xpos = Math.floor(Math.random() * canvas.width / 5) * 5 // bug picture x position
+        this.ypos = 0 // bug picture y position
+        this.width = 60 // bug picture width
+        this.height = 60 // bug picture height
+        this.speed = 5 // bug picture speed in animation
+        this.dy = 1 * this.speed // bug picture y axis movement
     }
 
     drawBug() {
-        context.drawImage(bugImage, this.xpos, this.ypos, this.width, this.height)
+        context.drawImage(bugImage, this.xpos, this.ypos, this.width, this.height) // drawing bug picture on canvas
     }
 
     update() {
-        this.drawBug()
-        this.ypos += this.dy
+        this.drawBug() // bug picture updating animation
+        this.ypos += this.dy // bug picture moving on y axis animation
     }
 }
 
-let leftKeyPress = false;
-let rightKeyPress = false;
-let zKeyPress = false;
-let cKeyPress = false;
+let leftKeyPress = false; // starting left key press falsy value
+let rightKeyPress = false; // starting right key press falsy value
+let zKeyPress = false; // starting z key press falsy value
+let cKeyPress = false; // starting c key press falsy value
 
-const leftKey = 37
-const rightKey = 39
-const zKey = 90
-const cKey = 67
+const leftKey = 37 // left arrow key code
+const rightKey = 39 // right arrow key code
+const zKey = 90 // z key code
+const cKey = 67 // c key code 
 
-function keyPressed(evt) {
-    if(evt.keyCode == leftKey) {
-        leftKeyPress = true
+function keyPressed(evt) { // key press function
+    if(evt.keyCode == leftKey) { // if left key is pressed
+        leftKeyPress = true // key press is set to true
     }
-    if(evt.keyCode == rightKey) {
-        rightKeyPress = true
+    if(evt.keyCode == rightKey) { // if right key is pressed
+        rightKeyPress = true // key press is set to true
     }
-    if(evt.keyCode == zKey) {
-        zKeyPress = true
+    if(evt.keyCode == zKey) { // if z key is pressed
+        zKeyPress = true // key press is set to true
     }
-    if(evt.keyCode == cKey) {
-        cKeyPress = true
+    if(evt.keyCode == cKey) { // if c key is pressed
+        cKeyPress = true // key press is set to true
     }
 }
 
 function keyReleased(evt) {
-    if(evt.keyCode == leftKey) {
-        leftKeyPress = false
+    if(evt.keyCode == leftKey) { // if left key is released
+        leftKeyPress = false // key press is set to false
     }
-    if(evt.keyCode == rightKey) {
-        rightKeyPress = false
+    if(evt.keyCode == rightKey) { // if right key is released
+        rightKeyPress = false // key press is set to false
     }
-    if(evt.keyCode == zKey) {
-        zKeyPress = false
+    if(evt.keyCode == zKey) { // if z key is released
+        zKeyPress = false // key press is set to false
     }
-    if(evt.keyCode == cKey) {
-        cKeyPress = false
+    if(evt.keyCode == cKey) { // if c key is released
+        cKeyPress = false // key press is set to false
     }
 }
 
 function rightWall() {
-    if(players.firstPlayer.xpos + players.firstPlayer.width > canvas.width){
-        players.firstPlayer.xpos = canvas.width - players.firstPlayer.width
+    if(players.firstPlayer.xpos + players.firstPlayer.width > canvas.width){ // if the first player's x position plus the the first player's width is more than the canvas width
+        players.firstPlayer.xpos = canvas.width - players.firstPlayer.width // the first player's x position is equal to the canvas width subtracted by the first player's width
         
     }
-    if(players.secondPlayer.xpos + players.secondPlayer.width > canvas.width){
-        players.secondPlayer.xpos = canvas.width - players.secondPlayer.width
+    if(players.secondPlayer.xpos + players.secondPlayer.width > canvas.width){ // if the second player's x position plus the the second player's width is more than the canvas width
+        players.secondPlayer.xpos = canvas.width - players.secondPlayer.width // the second player's x position is equal to the canvas width subtracted by the second player's width
     }
 }
 
 
 function playerMove() {
-    if(zKeyPress && players.firstPlayer.xpos > 0) {
-        players.firstPlayer.xpos -= players.firstPlayer.speed
+    if(zKeyPress && players.firstPlayer.xpos > 0) { // if the z key is pressed and the first player's x position is greater than 0
+        players.firstPlayer.xpos -= players.firstPlayer.speed // then the first player's x position is subtracted by the first player's speed
     }
-    if(cKeyPress) {
-        players.firstPlayer.xpos += players.firstPlayer.speed
+    if(cKeyPress) { // if the c key is pressed
+        players.firstPlayer.xpos += players.firstPlayer.speed // then the first player's x position is added by the first player's speed
     }
-    if(leftKeyPress && players.secondPlayer.xpos > 0) {
-        players.secondPlayer.xpos -= players.secondPlayer.speed
+    if(leftKeyPress && players.secondPlayer.xpos > 0) { // if the left arrow key is pressed and the second player's x position is greater than 0
+        players.secondPlayer.xpos -= players.secondPlayer.speed // then the second player's x position is subtracted by the second player's speed
     }
-    if(rightKeyPress) {
-        players.secondPlayer.xpos += players.secondPlayer.speed
+    if(rightKeyPress) { // if the right arrow key is pressed
+        players.secondPlayer.xpos += players.secondPlayer.speed // then the second player's x position is added by the second player's speed
     }
     
-    rightWall()
+    rightWall() // right wall collision function
 }
 
-function timer() {
-    if (time > 0) {
-        time -= 1;
-        h1.innerHTML = `Time: ${time}`;
-    }
-}
-
-function win() {
-    if (player1score > player2score) {
-        alert('Player One Wins!')
-    }
-
-    if (player2score > player1score) {
-        alert('Player Two Wins!')
-    }
-
-    if (player1score == player2score) {
-        alert('No winner! Game is tied!')
+function timer() { // timer function
+    if (time > 0) { // if the time is greater than 0
+        time -= 1; // then the time is subtracted by 1
+        h1.innerHTML = `Time: ${time}`; // and the header is changed from the title to the time limit
     }
 }
 
-function startGame() {
-    setInterval(() => {
-        bugArray.push(new Bug)
+function win() { // win function
+    if (player1score > player2score) { // if player 1's score is more than player 2's score
+        alert('Player One Wins!') // an alert pops up that declares player one the winner
+    }
+
+    if (player2score > player1score) { // if player 2's score is higher than player 1's score
+        alert('Player Two Wins!') // then an alert pops up that declares player two the winner
+    }
+
+    if (player1score == player2score) { // if player 1's score is equal to player 2's score
+        alert('No winner! Game is tied!') // then an alert pop's up declaring the game is a tie
+    }
+}
+
+function startGame() { // start game function
+    setInterval(() => { // every second
+        bugArray.push(new Bug) // a new bug is pushed into the bug array
     }, 1000)     
-    setInterval(() => {
-        timer()
-        if (time === 0) {
-            win()
+    setInterval(() => { // every second
+        timer() // the timer is decreased by 1
+        if (time === 0) { // if the time is equal to 0
+            win() // check the win function
         }
     }, 1000)
-    startButton.classList.add('hidden');
-    player1scoreText.classList.remove('hidden');
-    player2scoreText.classList.remove('hidden');
-    canvas.classList.remove('hidden')
+    startButton.classList.add('hidden'); // add hidden class and css to the start button
+    player1scoreText.classList.remove('hidden'); // remove the hidden class and css from the player 1 score text
+    player2scoreText.classList.remove('hidden'); // remove the hidden class and css from the player 2 score text
+    canvas.classList.remove('hidden') // remove the hidden class and css from the canvas
 }
 
-function bugMovement () {
-    bugArray.forEach((bug, index) => {
-        if (bug.ypos >= canvas.height || bug.xpos + bug.width > canvas.width) {
-            bugArray.splice(index, 1)
+function bugMovement () { // bug movement function
+    bugArray.forEach((bug, index) => { // for each bug in the bug array and it's index
+        if (bug.ypos >= canvas.height || bug.xpos + bug.width > canvas.width) { // if the bug's y position is greater than or equal to the canvas height or the bug's x position plus the bug's width is greater than the canvas width
+            bugArray.splice(index, 1) // remove that bug from the array
         }
-        else {
-            bug.update()
+        else { // if the above if statement isn't true
+            bug.update() // update the bug animation
         }    
     },
 
-    bugArray.forEach((bug, index) => {
-        if (players.firstPlayer.xpos == bug.xpos && players.firstPlayer.ypos < bug.ypos) {
-                bugArray.splice(index, 1)
-                player1score += 1
-                player1scoreText.innerHTML = `Player One Score<br>${player1score}`
+    bugArray.forEach((bug, index) => { // for each bug in the bug array and it's corresponding index
+        if (players.firstPlayer.xpos == bug.xpos && players.firstPlayer.ypos < bug.ypos) { // if the first player's x position is equal to the bug's x position and the first player's y position is less than the bug's y position
+                bugArray.splice(index, 1) // remove that bug from the bug array
+                player1score += 1 // increment player 1's score
+                player1scoreText.innerHTML = `Player One Score<br>${player1score}` // change the player 1 score text to show the current player one score
         }
 
-        if (players.secondPlayer.xpos == bug.xpos && players.secondPlayer.ypos < bug.ypos) {
-                bugArray.splice(index, 1)
-                player2score += 1
-                player2scoreText.innerHTML = `Player 2 Score<br>${player2score}`
+        if (players.secondPlayer.xpos == bug.xpos && players.secondPlayer.ypos < bug.ypos) { // if the second player's x position is equal to the bug's xposition and the second player's y position is less than the bug's y position
+                bugArray.splice(index, 1) // remove that bug from the bug array
+                player2score += 1 // increment player 2's score by 1
+                player2scoreText.innerHTML = `Player 2 Score<br>${player2score}` // change the player 2 score text to show the current player two score
         }
     })
 )}
     
 function clear() {
-        context.clearRect(0, 0, canvas.width, canvas.height)
+        context.clearRect(0, 0, canvas.width, canvas.height) // clear the canvas
 }
 
-function updateElements() {
-    clear()
-    players.drawPlayerOne()
-    players.drawPlayerTwo()
-    playerMove()
-    bugMovement()
-    requestAnimationFrame(updateElements)
+function updateElements() { // update all elements that need animation
+    clear() // clear the canvas
+    players.drawPlayerOne() // draw player one picture
+    players.drawPlayerTwo() // draw player 2 picture
+    playerMove() // move the players when keys are pressed
+    bugMovement() // move the bugs down the screen
+    requestAnimationFrame(updateElements) // animate all functions in the update elements function
 }
 
-document.addEventListener('keydown', keyPressed)
-document.addEventListener('keyup', keyReleased)
-startButton.addEventListener('click', () => { 
-    startGame();
-    timer()
-    updateElements()
+document.addEventListener('keydown', keyPressed) // when a key is pressed, run the key pressed function
+document.addEventListener('keyup', keyReleased) // when a key is released, run the key released function
+startButton.addEventListener('click', () => { // when start button is pressed
+    startGame(); // run the start game function
+    timer() // run the timer function
+    updateElements() // run the update elements function
 })
